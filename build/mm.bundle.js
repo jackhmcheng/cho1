b806143c56f3eef472d1991a08a740eef7f5e3d6
jackhmcheng <jack.hmcheng@gmail.com>
2017-06-28 16:41:42 +0800

    v2.0.8: fix reported issues (item 2)

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -6016,7 +6016,12 @@ angular.module('mm.core.courses', [])
 .config(["$stateProvider", function($stateProvider) {
     $stateProvider
     .state('site.mm_courses', {
-        url: '/mm_courses',
+        
+	// **** version 2.0.8 try fix reload of search result after enrol
+        cache: false,
+        // *** End ***    
+	    
+    	url: '/mm_courses',
         views: {
             'site': {
                 templateUrl: 'core/components/courses/templates/list.html',
@@ -6034,7 +6039,12 @@ angular.module('mm.core.courses', [])
         }
     })
     .state('site.mm_viewresult', {
-        url: '/mm_viewresult',
+        
+	// **** version 2.0.8 try fix reload of search result after enrol
+        cache: false,
+        // *** End ***    
+	    
+    	url: '/mm_viewresult',
         params: {
             course: null
         },
